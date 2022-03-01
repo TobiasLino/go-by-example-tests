b5c5359145335b0dc1bf05d74527026eba8b50e4
TobiasLino <linop019@gmail.com>
2022-03-01 20:49:19 -0300

    variables

---
+++ src/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Teste"
+	fmt.Println(a)
+
+	var b int
+	fmt.Println(b)
+
+	c := "teste2"
+	fmt.Println(c)
+
+	var d string
+	fmt.Println(d)
+}
